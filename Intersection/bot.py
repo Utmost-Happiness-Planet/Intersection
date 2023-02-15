5c0368265d5d54760576f1b9edf46c1c83040d67
MC_cubes <mccube2000@outlook.com>
2023-02-16 01:40:55 +0800

    import Adapter

--- Intersection/bot.py
+++ Intersection/bot.py
@@ -1,11 +1,13 @@
 import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as onebot
 
-from src.adapters.MCDR import Adapter
+from src.adapters.MCDR import Adapter as MCDR
 
 nonebot.init()
 
 driver = nonebot.get_driver()
-driver.register_adapter(Adapter)
+driver.register_adapter(MCDR)
+driver.register_adapter(onebot)
 nonebot.load_plugin("nonebot.plugins.echo")
 nonebot.load_plugin("nonebot.plugins.single_session")
 
