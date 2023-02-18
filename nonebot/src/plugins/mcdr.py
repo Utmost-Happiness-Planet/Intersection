19b1495be5b649d7eb994e7f68c4ed72c389179f
MC_cubes <mccube2000@outlook.com>
2023-02-18 20:25:24 +0800

    修改为接收事件

--- nonebot/src/plugins/mcdr.py
+++ nonebot/src/plugins/mcdr.py
@@ -1,24 +1,19 @@
 from nonebot import get_bot
-from nonebot.adapters import Bot, Message
-from nonebot.params import EventMessage
+from nonebot.adapters import Bot, Event
 from nonebot.plugin import on_message
-from nonebot.rule import to_me
 
 all = on_message()
 
 
 @all.handle()
-async def handle_echo(bot: Bot, message: Message = EventMessage()):
+async def handle_echo(bot: Bot, event: Event):
     if bot.self_id == "MCDR":
-        # await get_bot("MCDR").call_api('send_msg', message=message)
-        # await echo.send(message=message)
-        print(message.__repr__())
-        if message['type'] == 'player':
-            await get_bot("1875300947").call_api('send_msg',
-                                                user_id=1875300947,
-                                                group_id=1083188592,
-                                                message_type='group',
-                                                message=message)
+        print(event)
+        if event.type == 'player':
+            await get_bot("1875300947").call_api(
+                'send_msg',
+                group_id=1083188592,
+                message_type='group',
+                message=f'<{event.get_user_id()}> {event.get_message()}')
     else:
-        print(message.extract_plain_text())
-        await get_bot("MCDR").call_api('send_msg', message=message)
+        await get_bot("MCDR").call_api('send_msg', message=event.get_message())
